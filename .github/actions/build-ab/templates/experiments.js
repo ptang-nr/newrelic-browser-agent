6697cd88cac9e86adddf067ef496017bcb9e6531
Jordan Porter <silentsummitsoftware@gmail.com>
2024-04-17 08:47:28 -0600

    add autostart

--- .github/actions/build-ab/templates/experiments.js
+++ .github/actions/build-ab/templates/experiments.js
@@ -5,6 +5,7 @@ window.NREUM.info.applicationID = '{{{args.abAppId}}}'
 window.NREUM.info.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.init.proxy = {} // Proxy won't work for experiments
 window.NREUM.init.session_replay.enabled = true // enabled for ST migration experiment.  Follow up by disabling when the experiement is closed
+window.NREUM.init.session_replay.autoStart = true // enabled for ST migration experiment.  Follow up by disabling when the experiement is closed
 window.NREUM.init.session_trace.enabled = true // enabled for ST migration experiment.  Follow up by disabling when the experiement is closed
 window.NREUM.init.feature_flags = ['ajax_metrics_deny_list','soft_nav']
 
