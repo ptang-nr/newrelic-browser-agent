4ec435c0207c4048546ed9dd4ef254e3ae8eb721
Chunwai Li <cli@newrelic.com>
2024-11-06 16:13:44 -0600

    sab4

--- .github/actions/build-ab/templates/experiments.js
+++ .github/actions/build-ab/templates/experiments.js
@@ -52,8 +52,8 @@ window.NREUM.loader_config.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.info.applicationID = '{{{args.abAppId}}}'
 window.NREUM.info.licenseKey = '{{{args.abLicenseKey}}}'
 window.NREUM.init.proxy = {} // Proxy won't work for experiments
-window.NREUM.init.session_replay.enabled = false // disabled for now to not double wrap the page which can cause extra processing burden
-window.NREUM.init.session_trace.enabled = false // disabled for now to not double wrap the page which can cause extra processing burden
+// window.NREUM.init.session_replay.enabled = false // disabled for now to not double wrap the page which can cause extra processing burden
+// window.NREUM.init.session_trace.enabled = false // disabled for now to not double wrap the page which can cause extra processing burden
 window.NREUM.init.feature_flags = ['ajax_metrics_deny_list','soft_nav']
 
 {{#if experimentScripts}}
